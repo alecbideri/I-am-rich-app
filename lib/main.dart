1a669506a1fb01a55f19b1bf4c969a819c51aa63
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2024-04-24 12:26:47 +0200

    I am rich app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'I am Rich',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset('images/img_1.png'),
+        ),
+      ),
+    ),
+  );
+}
